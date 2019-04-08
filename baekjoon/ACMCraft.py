11abfa8e1fa0f894dd3a5913673482aa793a05b9
clap <clap@kdml.co.kr>
2019-04-08 17:01:42 +0900

    쇠막대기

--- baekjoon/ACMCraft.py
+++ baekjoon/ACMCraft.py
@@ -1,13 +1,10 @@
 import sys
 
 def solution():
-    test_cases = sys.stdin.readline()
-    for test_case in test_cases:
-        num_of_bldg = 0
-        num_of_rule = 0
-        construct_time = dict()
-        adj_dict = dict()
-        target_bldg = 0
+    test_cases = int(sys.stdin.readline())
+    answer = list()
+    for i in range(test_cases):
+        total_construct_time = dict()
 
         [num_of_bldg, num_of_rule] = map(int, sys.stdin.readline().split())
         construct_time = {idx+1: e for idx, e in enumerate(map(int, sys.stdin.readline().split()))}
@@ -19,7 +16,17 @@ def solution():
 
         target_bldg = int(sys.stdin.readline())
 
-        print(dfs(adj=adj_dict, start=1))
+        for n in adj_dict:
+            if n not in adj_dict.values():
+                total_construct_time[n] = construct_time[n]
+
+        topology = dfs(adj=adj_dict, start=1)
+        assign_construct_time_each_bldg(adj_dict, topology, construct_time, total_construct_time)
+
+        answer.append(total_construct_time[target_bldg])
+
+    for each in answer:
+        print(each)
 
 '''
 def dfs(adj, start):
@@ -44,25 +51,30 @@ def dfs_visit(adj, v, visited, parent):
     for e in adj[v]:
 '''
 
+def assign_construct_time_each_bldg(adj, topology, time_dict, total_time_dict):
+    for v in topology:
+        for u in adj[v]:
+            total_time_dict[u] = max(total_time_dict[v] + time_dict[u], total_time_dict[u])
+    return total_time_dict
 
 # with recursive
 def dfs(adj, start):
     visited = list()
     finished = list()
     dfs_visit(adj, start, visited, finished)
-    return visited
+    finished.reverse()
+    return finished
+    # return visited
 
 
 def dfs_visit(adj, v, visited, finished):
     visited.append(v)
     for u in adj[v]:
         if u not in visited:
-            dfs_visit(adj, u, visited)
+            dfs_visit(adj, u, visited, finished)
         elif u not in finished:
             cycle = True
-    finished.append(u)
-
-
+    finished.append(v)
 
 
 # without recursive
@@ -127,4 +139,62 @@ if __name__ == '__main__':
 6 7
 7 8
 7
+'''
+
+
+'''
+build_order = {}
+build_time = []
+answer_list = []
+def main():
+    global build_order
+    global build_time
+    case = int(input())
+    for idx in range(case):
+        row = input().split(" ")[1]
+        build_time = input().split(" ")
+        for jdx in range(int(row)):
+            order = input().split(" ")
+            start = str(order[0])
+            end = str(order[1])
+            if not build_order.get(end):
+                build_order[end] = {}
+                build_order[end]["parent"] = [start]
+            else:
+                if start not in build_order[end]["parent"]:
+                    build_order[end]["parent"].append(start)
+
+        target = input()
+
+        answer_list.append(max_time(target))
+        build_order = {}
+        build_time = []
+
+    for answer in answer_list:
+        print(answer)
+
+
+def max_time(build_number):
+    global build_order
+    global build_time
+
+    self_dict = build_order.get(build_number)
+    if not self_dict:
+        return int(build_time[int(build_number) - 1])
+    else:
+        if self_dict.get("maxtime"):
+            return self_dict.get("maxtime")
+
+        else:
+            mt = 0
+            for parent in self_dict["parent"]:
+                mt = max(mt,max_time(parent))
+
+        mt = mt + int(build_time[int(build_number) - 1])
+        self_dict["maxtime"] = mt
+        return mt
+
+
+if __name__ == "__main__":
+    main()
 '''
