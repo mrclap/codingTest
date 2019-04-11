732a71e675f3ea313efb7a0346969fa6f91d15f3
clap <clap@kdml.co.kr>
2019-04-11 18:38:06 +0900

    just beginning

---
+++ programmers/다리를지나는트럭2.py
@@ -0,0 +1,18 @@
+def solution(bridge_length, weight, truck_weights):
+
+    while truck_weights:
+        pass
+
+    answer = 0
+
+
+
+    return answer
+
+
+if __name__ == '__main__':
+    bridge_length = 2
+    weight = 100
+    truck_weights = [7, 4, 5, 6]
+
+    print(solution(bridge_length, weight, truck_weights))
