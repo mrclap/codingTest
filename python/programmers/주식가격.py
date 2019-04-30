f3af0ded2420579b2017b35704b000d472832a3a
clap <clap@kdml.co.kr>
2019-04-30 18:28:13 +0900

    주식가격

--- python/programmers/주식가격.py
+++ python/programmers/주식가격.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
+'''
+4/30 1차 시도
 
-
+'''
 def solution(p):
-    answer = [x for x in range(len(p)-1, -1, -1)]
-    for idx in range(len(p)-1, 0, -1):
-        for jdx in range(idx-1, -1, -1):
-            if p[jdx] > p[idx]:
-                answer[jdx] -= 1
+    answer = [0 for x in range(len(p))]
+    for idx in range(len(p)-1):
+        answer[idx] += 1
+        for jdx in range(idx+1, len(p)-1):
+            if p[jdx] >= p[idx]:
+                answer[idx] += 1
+            else:
+                break
 
     return answer
 
-
 if __name__ == '__main__':
-    prices = [1, 2, 3, 2, 3]
+    # prices = [1, 2, 3, 2, 3]
+    prices = [1, 1, 1, 1, 1]
     print(solution(prices))
