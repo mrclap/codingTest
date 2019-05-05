625c1b652e739c71d864c4692021d4298a99b0fa
Su-hyeong Park <mr.clap8@gmail.com>
2019-05-05 18:33:05 +0900

    라면공장

---
+++ programmers/라면공장.py
@@ -0,0 +1,11 @@
+def solution(stock, dates, supplies, k):
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    stock = 4
+    dates = [4, 10, 15]
+    supplies = [20, 5, 10]
+    k = 30
+
+    print(solution(stock, dates, supplies, k))
